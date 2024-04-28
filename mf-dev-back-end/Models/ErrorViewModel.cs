ea4a76edfa1aaac6d446ade9437a5b077bac64be
Riniel <rinielrodrigo555@gmail.com>
2024-04-28 02:44:02 -0300

    Adicionar arquivos de projeto.

---
+++ mf-dev-back-end/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mf_dev_back_end.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
